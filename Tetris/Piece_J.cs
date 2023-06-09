360011d37ab78e29634e1df760f7707160b8c3bc
Mark Eskenazi <markcrusher123@gmail.com>
2023-06-09 06:56:40 -0400

    Adding missing pieces: J and S

---
+++ Tetris/Piece_J.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Tetris
+{
+    public class Piece_J : PieceBase
+    {
+        public Piece_J()
+        {
+            pieceType = PieceType.J;
+            array2D = new int[,]
+            {
+                { 1, 0, 0 },
+                { 1, 1, 1 },
+                { 0, 0, 0 },
+            };
+            x_center_offset = 1;
+            y_center_offset = 1;
+
+        }
+    }
+}
+
---
+++ Tetris/Piece_S.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Tetris
+{
+    public class Piece_S : PieceBase
+    {
+        public Piece_S()
+        {
+            pieceType = PieceType.S;
+            array2D = new int[,]
+            {
+                { 0, 1, 1 },
+                { 1, 1, 0 },
+                { 0, 0, 0 },
+            };
+            x_center_offset = 1;
+            y_center_offset = 1;
+
+        }
+    }
+}
+
